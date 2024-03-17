8dd49d30a81908615e0d8c518bb95292c78db390
Farhad <farhadg@the-ifc.com>
2024-03-17 16:30:27 +0100

    cleaning

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 
+// generer un nombre aleatoire entre 0 et max
 function generateNumber(max) {
     return Math.floor(Math.random() * max);
 }
@@ -20,53 +21,42 @@ function onValidate(userInput, randomNumber) {
     }
 }
 
+// attendre que le DOM soit chargé
 document.addEventListener("DOMContentLoaded", () => {
 
-    let number = 2
-    number = number + 4
-    
-    let string = "resultat"
-    string = string + "test"
-
-    let input = document.getElementById("mon_input");
+    // element HTML <input> qui va contenir notre nombre
+    let input = document.getElementById("input");
 
     // element HTML <p> qui va contenir notre resultat
     let resultParagraph = document.getElementById("result");
 
-    // recuperer ce qui est de base dans le paragraphe
+    // recupere ce qui est de base dans le paragraphe
     let baseContent = resultParagraph.innerHTML;
 
+    // generer un nombre aleatoire
     let randomNumber = generateNumber(100);
+    console.log(randomNumber);
+
+    // initialiser la valeur de l'input
     let inputValue = 0;
 
+    // element HTML <button> qui va declencher la validation
     let button = document.getElementById("button");
+
+    // ajouter un ecouteur d'evenement sur le bouton, qui se declenche quand on clique dessus
     button.addEventListener("click", () => {
 
         // resultat de la fonction => ex: votre nombre est trop grand
         let result = onValidate(inputValue, randomNumber);
 
-        resultParagraph.style.display = "flex";
-
-        // modifier le paragraphe pour afficher "votre nombre est trop grand"
-
-        // Le contenu de base de notre paragraphe
-        // => resultParagraph.innerHTML
-
-        // reinitialiser resultParagraph
-        // resultParagraph.innerHTML = "Resultat: ";
-
-        // ici on a "Resultat: Votre nombre machin"
+        // afficher le resultat dans le paragraphe
         resultParagraph.innerHTML = baseContent + result;
     })
 
     input.addEventListener("input", (event) => {
         const value = event.target.value;
-        console.log('value is:', value);
+
+        // mettre a jour la valeur de l'input
         inputValue = value;
-
-        // reinitialiser l'input quand on appuie sur valider
     })
-
-    console.log('printing')
-    // console.log(inputValue)
 })
